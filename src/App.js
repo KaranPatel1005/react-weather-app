a333146f1ef20da2ec33f79ba6fd3c618f291d52
KaranPatel1005 <kgkaran97@gmail.com>
2021-06-24 20:29:05 +0530

    new react project created and updated files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import CityComponent from './modules/CityComponents';
+import WeatherComponents from './modules/WeatherInfoComponents';
+
+const API_KEY = "2ee4d26e057eb25102754947e9afd4d7";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+  box-shadow: 0 3px 6px 0 #555;
+  padding: 20px 10px;
+  border-radius: 4px;
+  width: 380px;
+  background: white;
+  font-family: 'Montserrat', sans-serif;
+`;
+
+const AppLabel = styled.div`
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response =
+      await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    updateWeather(response.data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppLabel>React Weather App</AppLabel>
+      {weather ? (
+        <WeatherComponents weather={weather} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+
+    </Container>
   );
 }
 
